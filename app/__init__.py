7cb48f41e7bad7d9b34e438789e7cbd6e9beaebc
Vitor Esteves <vesteves33@gmail.com>
2021-09-19 02:40:03 -0300

    acrescentando os novos arquivos

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def home(name='Vitor'):
     return render_template('index.html', name=name)
--- app/task.py
+++ app/task.py
@@ -1,6 +1,46 @@
 class Task:
-    id = ''
-    type = ''
-    title = ''
-    description = ''
-    status = ''
+  
+  def __init__(self, title, description):
+      self.title = title
+      self.description = description
+
+      def getTitle(self):
+          return self.title
+
+      def getDescription(self):
+          return self.description
+
+      def setTitle(self, title):
+          self.title = title
+
+      def setDescription(self, description):
+          self.description = description  
+
+    
+  class TaskManager:
+      def createTask():
+          pass
+
+      def readTask():
+          pass
+      
+      def updateTask():
+          pass
+    
+      def deleteTask():
+          pass
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+id = ''
+type = ''
+title = ''
+description = ''
+status = ''
