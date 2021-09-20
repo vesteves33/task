98da6b496ff91530e96667c569cf8d4f241c1842
Vitor Esteves <vesteves33@gmail.com>
2021-09-20 16:11:44 -0300

    atualizando os arquivos

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
-def home(name='Vitor'):
-    return render_template('index.html', name=name)
+def home():
+    return render_template('index.html')
