679414ce0fd3eaf454847968637f10e41fbd03c4
Vitor Esteves <vesteves33@gmail.com>
2021-09-20 18:32:27 -0300

    começando a trabalhar um pouco de organizaçao e arquitetura da aplicaçao para um padrao MVC

---
+++ app/controller/taskmanager.py
@@ -0,0 +1,12 @@
+class TaskManager:
+      def createTask():
+          pass
+
+      def readTask():
+          pass
+      
+      def updateTask():
+          pass
+    
+      def deleteTask():
+          pass
