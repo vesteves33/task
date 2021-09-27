d7496c21ed5ae960a5f8a3f88c75b8da8f47b5d6
Vitor Esteves <vesteves33@gmail.com>
2021-09-26 22:05:53 -0300

    Reescrevendo o projeto utilizando css no lugar do Bs

--- app/routes/route.py
+++ app/routes/route.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from flask import render_template 
+from flask import render_template, url_for
 from app import app
 
 
 @app.route('/')
 def home():
-    return render_template('signup.html')
+    return render_template('header.html')
