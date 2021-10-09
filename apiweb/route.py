32d8a7add299d5bcb43d9b43e81c908e48d2a7c4
Vitor Esteves <vesteves33@gmail.com>
2021-10-09 03:27:45 -0300

    VEC-72 #tarefas atualizando os ultimos arquivos

--- apiweb/route.py
+++ apiweb/route.py
@@ -1,7 +1,8 @@
-from apiweb.controllers.taskController import Task
-from apiweb.controllers.userController import User
+from apiweb.controllers.controller import Task, User, Account,Login
 from apiweb import api
 
 #Rotas da aplicação puxando diretamente do diretório de controllers
 api.add_resource(Task, '/task/')
 api.add_resource(User, '/')
+api.add_resource(Account, '/create')
+api.add_resource(Login, '/login')
