e9646ae4dbb11e612969012dee9159e5941533e2
Vitor Esteves <vesteves33@gmail.com>
2021-10-08 21:24:30 -0300

    VEC-70 #comment adicionando atributo username no arquivo de model User

--- apiweb/models/User.py
+++ apiweb/models/User.py
@@ -7,10 +7,11 @@ from datetime import datetime
 
 class User(Document):
     id = ObjectIdField
-    name = StringField(min_length=3, max_length=30, required=True)
-    lastName = StringField(min_length=3, max_length=60, required=True)
+    name = StringField(min_length=3, max_length=30)
+    lastName = StringField(min_length=3, max_length=60)
     cpf = IntField(max_value=11, required=True)
     email = EmailField(allow_utf8_user=True)
-    password = StringField(min_length=6, max_length=16, regex='[A-Za-z0-9"]', )
+    username = StringField(required=True)
+    password = StringField(min_length=6, max_length=16, regex='[A-Za-z0-9"]')
     created_at = DateTimeField(default=datetime.utcnow)
     updated_at = DateTimeField(default=datetime.utcfromtimestamp)
