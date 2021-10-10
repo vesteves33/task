f49be2600bad3c56633a9f7e26a45608cf3c082d
Vitor Esteves <vesteves33@gmail.com>
2021-10-10 16:34:35 -0300

    VEC-85 #comment aplicando as mudanças feitas nas models por conta da troca do banco de dados realizada

--- apiweb/models/models.py
+++ apiweb/models/models.py
@@ -6,23 +6,33 @@ from datetime import datetime
 
 class User(database.Model):
     __tablename__ = 'tb_user'
-    id = database.Column(database.Integer(), primary_key=True)
-    name = database.Column()
-    lastName = database.Column()
-    cpf = database.Column()
-    email = database.Column()
-    username = database.Column()
-    password = database.Column()
-    created_at = database.Column(database.DateTime(default=datetime.utcnow()))
-    updated_at = database.Column(database.DateTime(default=datetime.timestamp()))
+    id = database.Column(database.Integer, primary_key=True)
+    name = database.Column(database.String(40), nullable=True)
+    lastName = database.Column(database.String(255), nullable=True)
+    cpf = database.Column(database.Integer, unique=True, nullable=True)
+    account = database.relationship('Account', backref='user', lazy=True)
+    tasks = database.relationship('Task', backref='user', lazy=True)
+    created_at = database.Column(database.DateTime, default=datetime.utcnow)
+    updated_at = database.Column(database.DateTime, default=datetime.timestamp)
 
 class Task(database.Model):
     __tablename__ = 'tb_task'
-    id = database.Column(database.Integer(), primary_key=True)
-    title = database.Column()
-    description = database.Column()
-    priority = database.Column(['Very high', 'High', 'Medium', 'Low', 'Very low'])
-    status = database.Column(['Backlog', 'In progress', 'Completed'])
-    created_at = database.Column() 
-    updated_at = database.Column()
+    id = database.Column(database.Integer, primary_key=True)
+    title = database.Column(database.String(40), nullable=False)
+    description = database.Column(database.Text(40), nullable=True)
+    priority = database.Column(database.String,  ['Very high', 'High', 'Medium', 'Low', 'Very low'])
+    status = database.Column(database.String, ['Backlog', 'In progress', 'Completed'])
+    created_at = database.Column(database.DateTime, default=datetime.utcnow) 
+    updated_at = database.Column(database.DateTime, default=datetime.timestamp)
+    user_id = database.Column(database.Integer, database.ForeignKey('user.id'), nullable=False)
 
+class Account(database.Model):
+    __tablename__ = 'tb_account'
+    id = database.Column(database.Integer, primary_key=True)
+    email = database.Column(database.String(255), unique=True, nullable=False)
+    username = database.Column(database.String(40), unique=True, nullable=False)
+    password = database.Column(database.String(16), nullable=False)
+    created_at = database.Column(database.DateTime,nullable=False, default=datetime.utcnow)
+    updated_at = database.Column(database.DateTime, nullable=True, default=datetime.timestamp)
+    #ForeignKey representando relação da conta com usuário
+    user_id = database.Column(database.Integer, database.ForeignKey('user.id'), nullable=False)
