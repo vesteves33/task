a1d9123551c533c31750f6b1fa71fff754bf1e58
Vitor Esteves <vesteves33@gmail.com>
2021-10-11 11:24:39 -0300

    VEC-66 #comment atualizacao do codigo iniciando aplicacao e banco de dados

--- apiweb/__init__.py
+++ apiweb/__init__.py
@@ -2,23 +2,29 @@ from flask import Flask,Blueprint, render_template, make_response
 from flask_restful import Api, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from pathlib import Path
+
+
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage/db_taskmanager.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
 api_bp = Blueprint('api',__name__)
 api = Api(api_bp)
 bcrypt = Bcrypt(app)
 database = SQLAlchemy(app)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage/db_taskmanager.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
 #rotas da aplicação
 from apiweb import route
 app.register_blueprint(api_bp)
+from apiweb.models.models import init_db
 
-
-
-
-
-
+#Criando o banco de dados ao iniciar aplicação com todas as Models inseridas e configuradas
+path = Path.cwd().joinpath('/apiweb/storage/').is_file()
+if path == False:
+    init_db(database)
+#EXEMPLO DE HASH DA BCRYPT
+#senha = 'xpto'
+#hashSenha = bcrypt.generate_password_hash(senha) -> Criando o hash da informação
+#verificaHash = bcrypt.check_password_hash(hashSenha, senha) -> Validando o valor do hash
