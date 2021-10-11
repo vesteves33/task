db0cef03580cd7d14ee4b54b6d67ce53e6de3234
Vitor Esteves <vesteves33@gmail.com>
2021-10-11 11:23:27 -0300

    VEC-85 #done #comment codigo revisado, reestruturado e configurado para as tabelas serem criadas ao iniciar a aplicacao

--- apiweb/models/models.py
+++ apiweb/models/models.py
@@ -4,6 +4,22 @@ from datetime import datetime
 
 #Aqui estarão as classes que serão representações de tabelas no BD
 
+class Database:
+    def save(self):
+        database.session.add(self)
+        database.session.commit()
+
+    def listAll(self):
+        database.query.all()
+
+    def listOne(self):
+        database.query.filter_by(self).first()
+
+    def delete(self):
+        database.session.delete(self)
+        database.session.commit()
+
+
 class User(database.Model):
     __tablename__ = 'tb_user'
     id = database.Column(database.Integer, primary_key=True)
@@ -19,12 +35,12 @@ class Task(database.Model):
     __tablename__ = 'tb_task'
     id = database.Column(database.Integer, primary_key=True)
     title = database.Column(database.String(40), nullable=False)
-    description = database.Column(database.Text(40), nullable=True)
-    priority = database.Column(database.String,  ['Very high', 'High', 'Medium', 'Low', 'Very low'])
-    status = database.Column(database.String, ['Backlog', 'In progress', 'Completed'])
+    description = database.Column(database.Text, nullable=True)
+    priority = database.Column(database.String, default="Medium") #Valores do Campo no banco -> ['Very high', 'High', 'Medium', 'Low', 'Very low']
+    status = database.Column(database.String, default="Backlog") #Valores do Campo no banco -> ['Backlog', 'In progress', 'Completed']
     created_at = database.Column(database.DateTime, default=datetime.utcnow) 
     updated_at = database.Column(database.DateTime, default=datetime.timestamp)
-    user_id = database.Column(database.Integer, database.ForeignKey('user.id'), nullable=False)
+    user_id = database.Column(database.Integer, database.ForeignKey('tb_user.id'), nullable=False)
 
 class Account(database.Model):
     __tablename__ = 'tb_account'
@@ -35,4 +51,7 @@ class Account(database.Model):
     created_at = database.Column(database.DateTime,nullable=False, default=datetime.utcnow)
     updated_at = database.Column(database.DateTime, nullable=True, default=datetime.timestamp)
     #ForeignKey representando relação da conta com usuário
-    user_id = database.Column(database.Integer, database.ForeignKey('user.id'), nullable=False)
+    user_id = database.Column(database.Integer, database.ForeignKey('tb_user.id'), nullable=False)
+
+def init_db(self):
+    self.create_all()
