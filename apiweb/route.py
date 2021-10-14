874268f5377f306faf621916daafc913588baedf
Vitor Esteves <vesteves33@gmail.com>
2021-10-13 21:43:45 -0300

    VEC-86 #tarefas #comment modificando arquivo de rotas com base nas mudancas feito na controller

--- apiweb/route.py
+++ apiweb/route.py
@@ -1,8 +1,8 @@
-from apiweb.controllers.controller import Task, User, Account,Login
+from apiweb.controllers.controller import Task, User, NewAccount,NewLogin
 from apiweb import api
 
 #Rotas da aplicação puxando diretamente do diretório de controllers
 api.add_resource(Task, '/task/')
 api.add_resource(User, '/')
-api.add_resource(Account, '/create')
-api.add_resource(Login, '/login')
+api.add_resource(NewAccount, '/create')
+api.add_resource(NewLogin, '/login/<string:email>', '/login')
