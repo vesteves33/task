86e30465a6217253e4355b60131a661429510875
Vitor Esteves <vesteves33@gmail.com>
2021-10-14 01:01:48 -0300

    VEC-86 #comment novas rotas criadas, com views provisorias ate area autenticada ser feita

--- apiweb/route.py
+++ apiweb/route.py
@@ -1,8 +1,10 @@
-from apiweb.controllers.controller import Task, User, NewAccount,NewLogin
+from apiweb.controllers.controller import Home, NewListTask, NewTask, NewUser, NewAccount,NewLogin
 from apiweb import api
 
 #Rotas da aplicação puxando diretamente do diretório de controllers
-api.add_resource(Task, '/task/')
-api.add_resource(User, '/')
+api.add_resource(Home, '/')
+api.add_resource(NewListTask, '/tasks')
+api.add_resource(NewTask, '/task/', '/task/<int:id>')
+api.add_resource(NewUser, '/perfil')
 api.add_resource(NewAccount, '/create')
-api.add_resource(NewLogin, '/login/<string:email>', '/login')
+api.add_resource(NewLogin, '/login')
