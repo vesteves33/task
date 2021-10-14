1dbc8b7644820e8d5822a42af0d2fb6e85784701
Vitor Esteves <vesteves33@gmail.com>
2021-10-14 01:00:57 -0300

    VEC-87 #done #comment arquivo init modificado para ter o bd carregado caso nao exista

--- apiweb/__init__.py
+++ apiweb/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask,Blueprint, render_template, make_response
+from flask import Flask, render_template, make_response
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from flask_restful import Api, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -10,21 +11,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = hashlib.sha256().digest()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage/db_taskmanager.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['JWT_SECRET_KEY'] = hashlib.sha256().digest()
 
-api_bp = Blueprint('api',__name__)
-api = Api(api_bp)
+api = Api(app)
 bcrypt = Bcrypt(app)
 database = SQLAlchemy(app)
+jwt = JWTManager(app)
 #rotas da aplicação
 from apiweb import route
-app.register_blueprint(api_bp)
 from apiweb.models.models import init_db
 
 #Criando o banco de dados ao iniciar aplicação com todas as Models inseridas e configuradas
 path = Path.cwd().joinpath('/apiweb/storage/').is_file()
 if path == False:
     init_db(database)
-#EXEMPLO DE HASH DA BCRYPT
-#senha = 'xpto'
-#hashSenha = bcrypt.generate_password_hash(senha) -> Criando o hash da informação
-#verificaHash = bcrypt.check_password_hash(hashSenha, senha) -> Validando o valor do hash
