f899f82fa327e42c8192fd35eda84dafb56e3d94
Vitor Esteves <vesteves33@gmail.com>
2021-10-13 21:39:06 -0300

    VEC-79 #comment atualizando controllers e rotas para logica de criacao de conta e login

--- apiweb/controllers/controller.py
+++ apiweb/controllers/controller.py
@@ -1,9 +1,10 @@
+from flask.helpers import url_for
 from flask_restful import Resource
-from flask import render_template, make_response, request
-from werkzeug.utils import redirect
-from apiweb.models.models import User
-from apiweb import database
-import json
+from flask import render_template, make_response, request, flash, redirect, url_for
+from apiweb.models.models import Account, User, save
+from apiweb import bcrypt
+from apiweb.formularios.forms import CreateAccount, Login
+
 
 class Task(Resource):
     def get(self):
@@ -16,27 +17,45 @@ class User(Resource):
     def get(self):
         return make_response(render_template('index.html'))
 
-class Account(Resource):
+class NewAccount(Resource):
     def get(self):
         headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('signup.html'), 200, headers)
+        createAccount = CreateAccount()
+        return make_response(render_template('signup.html', createAccount=createAccount), 200, headers)
 
     def post(self):
-        response = request.form.to_dict()
-        user = database.User.insert_one(response)
+        createAccount = CreateAccount()
         
-        headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('signup.html'),headers)
+        if createAccount.validate_on_submit() and 'create' in request.form:
+            password_hash = bcrypt.generate_password_hash(createAccount.password.data)
 
-class Login(Resource):
+            account = Account(email=createAccount.email.data, password=password_hash)
+            
+            save(account)
+
+            headers = {'Content-Type': 'text/html'}
+
+            flash(f'Conta criada com sucesso para o Email: {createAccount.email.data}', 'alert-success')
+            return make_response(render_template('signup.html', createAccount=createAccount), headers)
+
+        
+
+class NewLogin(Resource):
     def get(self):
+        login = Login()
         headers = {'Content-type': 'text/html'}
-        return make_response(render_template('login.html'), 200, headers)
+        return make_response(render_template('login.html',login=login), 200, headers)
     
     def post(self):
-        response = request.form.to_dict()
-        return response
-        
+        login = Login()
+
+        if login.validate_on_submit() and 'login' in request.form:
+            account = Account.query.filter_by(email=login.email.data).first()
+
+            if account and bcrypt.check_password_hash(account.password, login.password.data):
+                flash(f'Login feito com sucesso!')
+                headers = {'Content-type': 'text/html'}
+                return make_response(render_template('login.html', login=login), headers)
 
         
         
