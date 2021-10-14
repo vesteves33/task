fb79c78a4985ed29ccae02f5c6c7be729f9167ca
Vitor Esteves <vesteves33@gmail.com>
2021-10-14 01:02:41 -0300

    VEC-82 #comment novos forms, criacao de perfil e criacao de tarefa com rotas temporarias ja sendo criados

--- apiweb/formularios/forms.py
+++ apiweb/formularios/forms.py
@@ -1,16 +1,30 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, PasswordField, SubmitField
+from wtforms.fields.core import IntegerField, SelectField
+from wtforms.fields.simple import TextAreaField
 from wtforms.validators import DataRequired, Email, Length
 
 
 class CreateAccount(FlaskForm):
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    email = StringField('E-mail', validators=[DataRequired(), Email(message='Not a valid email adress.')])
     password = PasswordField('Password', validators=[DataRequired(), Length(6,16)])
-    create = SubmitField('Create')
+    create = SubmitField('Create account')
 
 
 class Login(FlaskForm):
-    email = StringField('E-mail', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email(message='Email not found, try again.')])
     password = PasswordField('Password', validators=[DataRequired(), Length(6,16)])
     lembrete = BooleanField('Keep me conected')
     login = SubmitField('Login')
+
+class CreateProfile(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(max=40)])
+    lastName = StringField('Last name', validators=[DataRequired(), Length(max=255)])
+    cpf = IntegerField('Cpf', validators=[DataRequired(), Length(max=15)])
+    profile = SubmitField('Create profile')
+
+class CreateTask(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(max=40)])
+    description = TextAreaField('Description')
+    priority = SelectField('Priority', choices=[('none','None'), ('very-high','Very High'), ('high','High'), ('medium', 'Medium'), ('low', 'Low'), ('very-low', 'Very low')])
+    task = SubmitField('Create task')
